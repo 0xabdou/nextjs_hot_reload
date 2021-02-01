4f2eb52524a5eca589025041b75971bc319c833b
aouahib <ouahibdou@gmail.com>
2021-02-01 17:38:11 +0100

    Minimal2

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,23 +1,7 @@
-import {shallowEqual, useDispatch, useSelector} from "react-redux";
-import {AppState} from "../store/store";
-import React, {useEffect} from "react";
-import LoginScreen from "../features/auth/ui/login-screen";
-import {getAuthUser} from "../features/auth/auth-slice";
-import LoggedInScreen from "../features/user/ui/logged-in-screen";
+import React from "react";
 
 const Home = () => {
-  const state = useSelector((state: AppState) => state.auth, shallowEqual);
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-    dispatch(getAuthUser());
-  }, []);
-
-  if (!state.initialized)
-    return <p>Loading...</p>;
-  if (state.authUser)
-    return <LoggedInScreen/>;
-  return <LoginScreen/>;
+  return <p>Help please!!</p>;
 };
 
 
